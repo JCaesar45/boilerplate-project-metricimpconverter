1e20aeee9bfd24434e4dc1a6f4db91b6ee7e57aa
Jordan Leturgez <jordanleturgez@gmail.com>
2025-06-24 01:17:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const apiRoutes = require("./routes/api");
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,9 +15,9 @@ app.get("/", (req, res) => {
   res.send("Metric-Imperial Converter API");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app; // for testing
